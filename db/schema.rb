195b4090ab08e9ba03482c1f88bfba0e1e8116df
bran.liang <bran.liang@shopify.com>
2021-09-26 11:33:24 +0800

    Add styles to signup

--- db/schema.rb
+++ db/schema.rb
@@ -56,6 +56,7 @@ ActiveRecord::Schema.define(version: 2021_09_26_004532) do
     t.string "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string "unconfirmed_email"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
